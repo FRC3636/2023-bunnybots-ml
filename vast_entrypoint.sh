19fed7a067fdb4fe89cccfdd7f633f6faf68150a
Max Niederman <max@maxniederman.com>
2023-11-03 23:06:45 -0700

    add debug prints to vast entrypoint

--- vast_entrypoint.sh
+++ vast_entrypoint.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+echo "Entered bunnybots vast.ai env."
+
 ssh-keygen -A
+echo "Generated host keys..."
 
 mkdir /root/.ssh
 echo $SSH_PUBLIC_KEY > /root/.ssh/authorized_keys
+echo "Authorized public key '${SSH_PUBLIC_KEY}'" 
 
 rm -rf /run/nologin
 
+echo "Starting sshd..."
 /usr/sbin/sshd -D
